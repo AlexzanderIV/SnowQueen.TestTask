20340704924b4209ac8e2f37e6c18f4efc879911
Alexander Seredin <alexander.seredin@saritasa.com>
2018-09-26 18:37:02 +0700

    Add WPF DataComparer project

---
+++ SnowQueen.TestTask.DataComparer/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SnowQueen.TestTask.DataAccess.Services;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SnowQueen.TestTask.DataComparer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // TODO: Call WCF-service
+
+            using (var service = 
+                ProductsServiceFactory.CreateWithFileRepository(ConfigurationManager.AppSettings["FileStoragePath"]))
+            {
+                var productsFromFile = service.GetAllProducts();
+            }
+        }
+    }
+}
